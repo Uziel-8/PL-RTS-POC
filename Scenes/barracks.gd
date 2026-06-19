ed7ab8c4e266cae6b25bc695b80c53c474c24335
Uziel-8 <blade.ruger@gmail.com>
2026-06-19 17:06:34 +0930

    Changed spawn mechanic to have a slight randomness on initial spawn location, to get rid of the queue that was forming, to help with testing. Added upgrade unit buttons, and a label that displays the relevant unit stats

--- Scenes/barracks.gd
+++ Scenes/barracks.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 func _on_spawn(UnitData):
 	var unit = preload("res://Units/player_footman.tscn").instantiate()
 	unit.unit_data = UnitData
-	unit.position = self.position
+	unit.position = self.position + Vector2(randi_range(100, -100), randi_range(100, -100))
 	get_parent().add_child(unit)
 	EventBus.gold_changed.emit(unit.unit_data.gold_cost)
 	print(unit.unit_data.name)
--- Scenes/hud.gd
+++ Scenes/hud.gd
@@ -2,11 +2,16 @@ extends Control
 
 
 @onready var gold_label: Label = $Panel2/GoldLabel
+@onready var FOOTMAN_DATA = preload("uid://uucgbpnkvr3b")
+@onready var ARCHER_DATA = preload("uid://qqqain4h0vo1")
+@onready var unit_stats_label: Label = $UnitStatsPanel/UnitStatsLabel
 
 func _ready() -> void:
 	gold_label.text = str("Player Gold: ", Globals.player_gold)
 	
 	EventBus.gold_changed.connect(_on_gold_changed)
+	
+	_stats_updated()
 
 func _on_footman_button_pressed() -> void:
 	_spawn(Globals.FOOTMAN)
@@ -21,3 +26,23 @@ func _spawn(unit_data: UnitData):
 func _on_gold_changed(change: int):
 	Globals.player_gold += change
 	gold_label.text = str("Player Gold: ", Globals.player_gold)
+
+func _stats_updated():
+	unit_stats_label.text = str(FOOTMAN_DATA.name, ": Damage: ", FOOTMAN_DATA.damage, ", Health: ", FOOTMAN_DATA.health, ", Speed: ", FOOTMAN_DATA.speed) + "\n" + str(ARCHER_DATA.name, ": Damage: ", ARCHER_DATA.damage, ", Health: ", ARCHER_DATA.health, ", Speed: ", ARCHER_DATA.speed)
+
+
+func _on_upgrade_footmen_pressed() -> void:
+	var cost = 10
+	if Globals.player_gold >= cost:
+		FOOTMAN_DATA.damage += 10
+		FOOTMAN_DATA.speed += 10
+		EventBus.gold_changed.emit(-cost)
+		_stats_updated()
+
+func _on_upgrade_archer_pressed() -> void:
+	var cost = 10
+	if Globals.player_gold >= cost:
+		ARCHER_DATA.damage += 10
+		ARCHER_DATA.speed += 10
+		EventBus.gold_changed.emit(-cost)
+		_stats_updated()
