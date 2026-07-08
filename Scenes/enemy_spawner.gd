87bbed41b67dbabbc4c9364e80be62f7117dd3a1
Uziel-8 <blade.ruger@gmail.com>
2026-07-08 17:04:37 +0930

    Added navigation for enemies - nav region, nav agents etc. no biggie.

--- Scenes/enemy_spawner.gd
+++ Scenes/enemy_spawner.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
 @onready var spawn_timer: Timer = $SpawnTimer
-
+@export var goal: Node = null
 
 
 func _on_spawn_timer_timeout() -> void:
 	var unit = preload("res://Units/enemy_footman.tscn").instantiate()
 	unit.unit_data = preload("res://Resources/slime_data.tres")
 	unit.position = self.position
+	unit.goal = goal
 	get_parent().add_child(unit)
--- Units/enemy_footman.gd
+++ Units/enemy_footman.gd
@@ -1,14 +1,23 @@
 extends CharacterBody2D
 
 @export var unit_data: UnitData
-
+@export var goal: Node = null
 @onready var health = unit_data.health
+
+@onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
+
 
 signal died
 
+func _ready() -> void:
+	nav_agent.target_position = goal.global_position
+
+
+
 func _physics_process(delta: float) -> void:
-	velocity.x = -50.0
-	
+	if !nav_agent.is_target_reached():
+		var nav_point_direction = to_local(nav_agent.get_next_path_position()).normalized()
+		velocity = nav_point_direction * unit_data.speed
 	
 	move_and_slide()
 
@@ -27,3 +36,13 @@ func _die():
 	EventBus.gold_changed.emit(unit_data.gold_bounty)
 	self.queue_free()
 	died.emit()
+
+
+func _on_nav_timer_timeout() -> void:
+	#if goal == null:
+		#for scene in get_tree():
+			#if scene.is_in_group("PlayerUnits"):
+				#goal = scene
+	if nav_agent.target_position != goal.global_position:
+		nav_agent.target_position = goal.global_position
+	$NavTimer.start()
